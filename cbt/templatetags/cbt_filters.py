7e2f435c4b6a82462057f2ca3f94276b51a380f2
Daarul Portal Admin <dev@daaraulportal.local>
2026-05-22 15:37:50 +0100

    cbt5

--- cbt/templatetags/cbt_filters.py
+++ cbt/templatetags/cbt_filters.py
@@ -3,21 +3,10 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='replace')
-def replace(value, args):
-    """Replace substrings in templates using syntax: value|replace:"old,new".
-
-    Example: {{ 'hello_world'|replace:'_ , ' }} -> 'hello world'
-    Args should be a single string with the old and new values separated by a comma.
-    """
+@register.filter(name='underscore_to_space')
+def underscore_to_space(value):
+    """Convert underscores in a string to spaces."""
     try:
-        old, new = [p for p in args.split(',')]
-    except Exception:
-        return value
-    # strip optional whitespace
-    old = old.strip()
-    new = new.strip()
-    try:
-        return value.replace(old, new)
+        return value.replace('_', ' ')
     except Exception:
         return value
