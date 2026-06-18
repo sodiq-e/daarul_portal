d8e5212b57947c3141529fd69ecfa1957d1077ef
sodiq-e <afolabisodiqabioye@gmail.com>
2026-06-18 16:45:01 +0000

    latest portal4

--- daarul_portal/settings.py
+++ daarul_portal/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'daarul_portal.urls'
 TEMPLATES = [
