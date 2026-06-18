845a42046a7fef99b2dadb6b8c17e467bb073f4c
sodiq-e <afolabisodiqabioye@gmail.com>
2026-06-18 22:14:37 +0000

    cloud1

--- daarul_portal/settings.py
+++ daarul_portal/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'cloudinary'
+     'cloudinary',
      'cloudinary_storage',
 
     'rest_framework',
